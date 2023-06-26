6720ef639729f3a506f9c3028c999c3eab868725
AngusBethke <55188747+AngusBethke@users.noreply.github.com>
2023-06-26 10:46:24 +0200

    0.4.3
    - Started work on parkour roof experience

---
+++ WhatRemains/source/data/joblists/mapssingle/rs_wroh_testground.jl
@@ -0,0 +1,56 @@
+!BackupDirectory( s )
+!LogFile( s )
+!Action( s )
+!ActionSpecData()
+!attrib( s, s )
+!attrib_custom( s, s )
+!member()
+!member_name( s )
+!member_enum( i )
+!member_enum_string( i, s )
+!member_value()
+//only first char of string is taken
+!char_member_value( s )
+//valid values: [0, 255]
+!byte_member_value( n )
+!int_member_value( n )
+//values have context meaning - be careful
+!objectref_member_value( n )
+!float_member_value( f )
+!string_member_value( s )
+!bool_member_value( n )
+!objectreflist_member_value( s )
+
+BackupDirectory( "joblist/backups" )
+$LOG_FILE(s, "joblists/logs/joblist_rs_wroh_testground.log")
+LogFile( LOG_FILE )
+
+//
+// Map file
+//
+$MAP_FILE (s, "maps/rs_wroh_testground/rs_wroh_testground.map")
+
+//
+// Gather resources
+//
+$PACK_FILE (s, "maps/rs_wroh_testground/rs_wroh_testground.rscr")
+$RLOG0 (s, "common.rlog")
+$RLOG1 (s, "rs_wroh_testground_resources.scr")
+$RLOG2 (s, "")
+$RLOG3 (s, "")
+$RLOG4 (s, "")
+$RLOGEXCL0 (s, "weapons.rscr")
+$RLOGEXCL1 (s, "common_meshes.rscr")
+$RLOGEXCL2 (s, "engine.rscr")
+$RLOGEXCL3 (s, "common_exclude.rscr")
+$RLOGEXCL4 (s, "common_anims.rscr")
+$RLOGEXCL5 (s, "")
+$RLOGEXCL6 (s, "")
+$RLOGEXCL7 (s, "")
+$OVERRIDE_SCRIPT (s, "rs_wroh_testground_ovr.scr")
+
+//auto-generated - DO NOT REMOVE THIS LINE
+
+!include("jl_options.jlt")
+!include("jl_configs.jlt")
+!include("level.jlt")
