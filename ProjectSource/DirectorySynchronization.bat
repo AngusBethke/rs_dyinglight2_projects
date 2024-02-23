61e5a6f3ac885c99c7d932b3abd68e70c57611cb
AngusBethke <55188747+AngusBethke@users.noreply.github.com>
2024-02-23 17:24:10 +0100

    Initial Commit
    Initial Commit

---
+++ ProjectSource/DirectorySynchronization.bat
@@ -0,0 +1,7 @@
+robocopy "D:\SteamLibrary\steamapps\common\Dying Light 2\DevTools\projects\local\CaveDweller\source\data\Prefabs" "D:\SteamLibrary\steamapps\common\Dying Light 2\DevTools\projects\local\ProjectSource\Prefabs" /E /MIR
+
+robocopy "D:\SteamLibrary\steamapps\common\Dying Light 2\DevTools\projects\local\Intersection\source\data\Prefabs" "D:\SteamLibrary\steamapps\common\Dying Light 2\DevTools\projects\local\ProjectSource\Prefabs" /E /MIR
+
+robocopy "D:\SteamLibrary\steamapps\common\Dying Light 2\DevTools\projects\local\WhatRemains\source\data\Prefabs" "D:\SteamLibrary\steamapps\common\Dying Light 2\DevTools\projects\local\ProjectSource\Prefabs" /E /MIR
+
+robocopy "D:\SteamLibrary\steamapps\common\Dying Light 2\DevTools\projects\local\RSTestProject\source\data\Prefabs" "D:\SteamLibrary\steamapps\common\Dying Light 2\DevTools\projects\local\ProjectSource\Prefabs" /E /MIR
---
+++ ProjectSource/Readme/readme.md
@@ -0,0 +1,6 @@
+robocopy "D:\SteamLibrary\steamapps\common\Dying Light 2\DevTools\projects\local\CaveDweller\source\data\Prefabs" "D:\SteamLibrary\steamapps\common\Dying Light 2\DevTools\projects\local\ProjectSource\Prefabs" /E /MIR
+
+robocopy "D:\SteamLibrary\steamapps\common\Dying Light 2\DevTools\projects\local\Intersection\source\data\Prefabs" "D:\SteamLibrary\steamapps\common\Dying Light 2\DevTools\projects\local\ProjectSource\Prefabs" /E /MIR
+
+robocopy "D:\SteamLibrary\steamapps\common\Dying Light 2\DevTools\projects\local\WhatRemains\source\data\Prefabs" "D:\SteamLibrary\steamapps\common\Dying Light 2\DevTools\projects\local\ProjectSource\Prefabs" /E /MIR
+
