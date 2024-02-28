f37c3c2f513932537599a253183c61d2be2fd785
AngusBethke <55188747+AngusBethke@users.noreply.github.com>
2024-02-28 17:24:18 +0100

    Added some additional setup files
    Added some additional setup files

---
+++ ProjectSource/WorkingSetup/CaveDweller.Setup.bat
@@ -0,0 +1,6 @@
+set masterDirectory="D:\SteamLibrary\steamapps\common\Dying Light 2\DevTools\projects\local\ProjectSource\Prefabs"
+set sourceDirectory="D:\SteamLibrary\steamapps\common\Dying Light 2\DevTools\projects\local\CaveDweller\source\data\Prefabs"
+
+call "D:\SteamLibrary\steamapps\common\Dying Light 2\DevTools\projects\local\ProjectSource\WorkingSetup\_Setup.bat" %sourceDirectory% %masterDirectory%
+
+pause
---
+++ ProjectSource/WorkingSetup/RSTestProject.Teardown.bat
@@ -0,0 +1,6 @@
+set masterDirectory="D:\SteamLibrary\steamapps\common\Dying Light 2\DevTools\projects\local\ProjectSource\Prefabs"
+set sourceDirectory="D:\SteamLibrary\steamapps\common\Dying Light 2\DevTools\projects\local\RSTestProject\source\data\Prefabs"
+
+call "D:\SteamLibrary\steamapps\common\Dying Light 2\DevTools\projects\local\ProjectSource\WorkingSetup\_Teardown.bat" %sourceDirectory% %masterDirectory%
+
+pause
