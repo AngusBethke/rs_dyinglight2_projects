18342ef19e2e72b53d000c0240eb7c3d5bc53ac8
AngusBethke <55188747+AngusBethke@users.noreply.github.com>
2025-06-04 14:17:29 +0200

    Project adventure initial commit
    Project adventure initial commit

---
+++ ProjectSource/WorkingSetup/ProjectAdventure.Setup.bat
@@ -0,0 +1,6 @@
+set masterDirectory="D:\SteamLibrary\steamapps\common\Dying Light 2\DevTools\projects\local\ProjectSource\Prefabs"
+set sourceDirectory="D:\SteamLibrary\steamapps\common\Dying Light 2\DevTools\projects\local\project_adventure\source\data\Prefabs"
+
+call "D:\SteamLibrary\steamapps\common\Dying Light 2\DevTools\projects\local\ProjectSource\WorkingSetup\_Setup.bat" %sourceDirectory% %masterDirectory%
+
+pause
