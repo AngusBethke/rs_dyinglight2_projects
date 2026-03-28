773ba5faa192305d98e17e6afadf8bca9e8a3b2f
AngusBethke <55188747+AngusBethke@users.noreply.github.com>
2026-03-28 13:53:35 +0100

    Utility: Updated directory management
    - Updated directory management to include brushes and terrains

--- ProjectSource/WorkingSetup/_Teardown.bat
+++ ProjectSource/WorkingSetup/_Teardown.bat
@@ -1,5 +1,23 @@
 set sourceDirectory=%1
 set masterDirectory=%2
 
-rd /s /q %sourceDirectory%
-robocopy %masterDirectory% %sourceDirectory% /e
+:: Prefab directory
+set masterPrefabDirectory = %masterDirectory% + "\Prefabs"
+set sourcePrefabDirectory = %sourceDirectory% + "\Prefabs"
+
+rd /s /q %sourcePrefabDirectory%
+robocopy %masterPrefabDirectory% %sourcePrefabDirectory% /e
+
+:: Brushes directory
+set masterBrushesDirectory = %masterDirectory% + "\Brushes"
+set sourceBrushesDirectory = %sourceDirectory% + "\Brushes"
+
+rd /s /q %sourceBrushesDirectory%
+robocopy %masterBrushesDirectory% %sourceBrushesDirectory% /e
+
+:: Terrain directory
+set masterTerrainsDirectory = %masterDirectory% + "\Textures\terrains"
+set sourceTerrainsDirectory = %sourceDirectory% + "\Textures\terrains"
+
+rd /s /q %sourceTerrainsDirectory%
+robocopy %masterTerrainsDirectory% %sourceTerrainsDirectory% /e
